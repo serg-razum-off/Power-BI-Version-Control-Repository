120440e90ec800d143f7601eac909a4125b6b277
SergiyR <sergiy.razumov@gmail.com>
2022-08-25 18:35:28 +0300

    SR: minor changes to Mgm Qwr --> txt btw delimiters found

--- AdventureWorks pbix/AdvWorksDW2020/Model/queries/Internet Sales.m
+++ AdventureWorks pbix/AdvWorksDW2020/Model/queries/Internet Sales.m
@@ -1,12 +1,14 @@
 let
-    Specification = 
+    ObjectInfo = 
         [
             Object = "Internet Sales",
             Type = "Table",
             Link to Specification = "https://onedrive.live.com/view.aspx?resid=43FC8CA3B17868DD%21806&id=documents&wd=target%28VS%20Code.one%7C062DAB10-4A3E-4127-8D96-1AB4A4286FC7%2FDummyTask%3A%20Add%20Internet-Sales%7CC38A5B3B-DB39-4862-8800-09D041CB42B0%2F%29",
             Developer = "Sergii Razumov",
-            Due Date = "30/08/2022"
-
+            Due Date = "30/08/2022",
+            Sprint = "1,2,3",
+            Ready pct = "0.85",
+            PM Comment = ""
         ]
     in 
-Specification
+ObjectInfo
