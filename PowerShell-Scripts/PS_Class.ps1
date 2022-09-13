6dee337c8da815b95533e97b3f6a13384f1cf51d
SergiyR <sergiy.razumov@gmail.com>
2022-09-13 12:07:00 +0300

    SR [2022-09-13]: Refactor Scripts --> Class done --> all Scripts of pbi-tools usage are utilized as Methods of the PS_Class

--- PowerShell-Scripts/PS_Class.ps1
+++ PowerShell-Scripts/PS_Class.ps1
@@ -1,9 +1,10 @@
 class PBIX {
-<#
+    <#
         .AUTHOR
             sergiy.razumov@gmail.com
         .DESCRIPTION
-            PowerShell Class to handle interactions with pbi-tools --> JSON splitted .pbx
+            PowerShell Class to handle interactions with pbi-tools --> JSON splitted .pbx 
+            One Proj for one pbi file --> number of changes is high. Several pbixes in one proj will mess up VC
         .EXAMPLE
             $pbix = [pbix]::new()
             $pbix = [pbix]::new(10, 175, 325, $false)
@@ -13,14 +14,16 @@ class PBIX {
     [string]$projectRoot
     # for import tables and measures specification
     [array]$managementPlan
+    # address of pbix. $null if not yet compiled or extracted
+    [string]$pbix
+    [string]$pbit
 
     #TODO: For lining up visuals 
     [int]$filtersLine_Y;     
     [int]$firstLine_Y;     
     [int]$secondLine_Y
 
-    # for paramless Ctor. See TODO: on param Ctor -- use ommitable (below)
-    [bool]$verbose = $false  ##TODO: use this Prop for identification of level of details of Methods
+    [bool]$verbose = $false  # Set to $true for detailed output in $this.Methods()
     
     #============== #CONSTRUCTORS ===========================
     #def
@@ -37,7 +40,7 @@ class PBIX {
         [int]$filtersLine_Y, 
         [int]$firstLine_Y, 
         [int]$secondLine_Y,
-        [bool]$verbose = $null #TODO: make this param ommitable -- IF this is possible
+        [bool]$verbose = $null 
     ) {         
         $this.Init(
             "{'filtersLine_Y':$filtersLine_Y, 'firstLine_Y':$firstLine_Y, 'secondLine_Y':$secondLine_Y}",
@@ -47,24 +50,32 @@ class PBIX {
     
     #=============== #METHODS =============================    
     #-----------------------------------------------------
+    hidden [void] WriteVerbose([string]$message) {
+        # Printing Verbose messages
+        if ($this.verbose) { 
+            $VerbosePreference = "Continue" 
+            Write-Verbose( "$message" )
+            $VerbosePreference = 'SilentlyContinue' 
+        }           
+    }
     hidden [void] Init([string]$jprop, [bool]$Verbose) {
         <#
             .DESCRIPTION
                 Method for init required values of the Obj. Use $Verbose to set it desired output.
         #>
-        #  starting Environment        
-        if ($Verbose) { $VerbosePreference = "Continue" ; $this.verbose = $Verbose }        
-        Write-Verbose "=== Starting PBIX Cls Init ==="
+        if ($Verbose) {
+            $this.verbose = $true
+        }
+        $this.WriteVerbose("=== Starting PBIX Cls Init ===")
         
         # setting up required modules        
-        Write-Verbose ">>> Setting up Required Modules..."
+        $this.WriteVerbose(">>> Setting up Required Modules...")
         @('ImportExcel') | ForEach-Object {
             if (-not (Get-Module $_ -ListAvailable)) { Install-Module -Name $_ }
-            else { Write-Verbose "module '$_' is already installed..." }
-        }
+            else { $this.WriteVerbose( "module '$_' is already installed..." ) }
+        }        
         
-        
-        Write-Verbose ">>> Setting up Properties... "
+        $this.WriteVerbose( ">>> Setting up Properties... " )
         if ($jprop -ne "") {
             $junpacked = $jprop | ConvertFrom-Json
             
@@ -78,23 +89,24 @@ class PBIX {
             $this.secondLine_Y = 300
         }
         
-        Write-Verbose ">>> Updating Data from Management Excle file..."
+        $this.WriteVerbose( ">>> Updating Data from Management Excle file..." )
         # setting properties
         $this.projectRoot = (Get-ChildItem -Path ../.gitignore -r).DirectoryName
         $this.managementPlan = Import-Excel (Get-ChildItem -Path $this.projectRoot *plan.xlsx* -r) `
             -WorksheetName "Planned Objects" `
             -StartRow 3
         
+        $this.pbix = (Get-ChildItem -Path ../*.pbix -Recurse)
+        $this.pbit = (Get-ChildItem -Path ../*.pbit -Recurse)
         # Updating Tables in Manage Plan
         $this.UpdateManagementPlanTables();
         
         # setting personal aliases
-        Write-Verbose ">>> Setting personal Aliases..."
+        $this.WriteVerbose( ">>> Setting personal Aliases..." )
         Set-Alias -Name touch -Value New-Item -Scope Global
         
         # wrapping the Init up
-        Write-Verbose "=== PBIX Cls Init Completed ==="        
-        if ($Verbose) { $VerbosePreference = 'SilentlyContinue' }         
+        $this.WriteVerbose( "=== PBIX Cls Init Completed ===" )        
     }
     #-----------------------------------------------------
     [void] Build() {
@@ -104,11 +116,11 @@ class PBIX {
         #>    
 
         #SR: getting pbit
-        $pbit = Get-ChildItem -Path $this.projectRoot *.pbit -Recurse
-        $base_path = $pbit.DirectoryName
+        $this.pbit = Get-ChildItem -Path $this.projectRoot *.pbit -Recurse
+        $base_path = $this.pbit.DirectoryName
 
         #SR: getting metadata dir
-        $md_dir = ($pbit.FullName -split "\\" ) 
+        $md_dir = ($this.pbit.FullName -split "\\" ) 
         $md_dir = ($md_dir[$md_dir.Count - 1] -split ".pbit")
 
         #SR: compiling .pbit and launching it
@@ -117,23 +129,21 @@ class PBIX {
             -format PBIT -overwrite;     
 
         #SR: if having Errs while compile
-        $substring_list = @("Error", "Global")
-        if (($substring_list | ForEach-Object { ($res -join "").contains($_) }) -contains $true) {
-            Write-host ">>> Error: `n"; 
-            Write-host ($res -join " <<>> ")
+        $substrings_list = @("Error", "Global")
+        if (($substrings_list | ForEach-Object { ($res -join "").contains($_) }) -contains $true) {
+            Write-Host ">>> Error: `n"; 
+            Write-Host ($res -join " <<>> ")
             throw
         }
         else {
-            Write-host ">>> Compiled successfully: `n"; Write-host $("-" * 100)
-            Write-host "$res `n"; Write-host $("-" * 100)
+            $this.WriteVerbose(">>> Compiled successfully: `n"); $this.WriteVerbose( $("-" * 100)   )
+            $this.WriteVerbose("$res `n"); $this.WriteVerbose( $("-" * 100) )
         }
 
         #SR: launching
-        pbi-tools.exe launch-pbi $pbit.FullName
-        
+        pbi-tools.exe launch-pbi $this.pbit.FullName
     }
     [void] Launch() { $this.Launch("") } # method overload to solve omittable param. $pbixType=$null doesn't work
-
     [void] Launch($pbixType) {
         <#
             .DESCRIPTION
@@ -144,29 +154,41 @@ class PBIX {
         $trgFile = $null
 
         if ($pbixType -eq "" -or $pbixType -eq "pbix") {
-            $trgFile = Get-ChildItem *.pbix -Recurse
+            $trgFile = $this.pbix
         }
         elseif ($pbixType -eq "PBIT") {
-            $trgFile = Get-ChildItem *.pbit -Recurse
+            $trgFile = $this.pbit
         }
         else {
             Write-Output ">> Wrong type of the Power BI file entered..."
         }
-
-        $fileName = $trgFile.Name
 
         if ($null -eq $trgFile) {
             Write-Host "`n >>> No file '$trgFile' found... `n"
             throw
         }
         
-        pbi-tools.exe launch-pbi $trgFile.FullName
+        $fileName = $trgFile.Split("\\")
+        $fileName = $fileName[$fileName.Count - 1]
+        
+        pbi-tools.exe launch-pbi $trgFile
 
-        if ($this.verbose) {
-            Write-Host ">>> File '$fileName' was launched..."
-        }
+        $this.WriteVerbose( ">>> File '$fileName' was launched..." )
     }
-    [void] WatchMode() {}
+    [void] WatchMode() {
+        #SR: Turning ON the watch mode
+        $PrId = (pbi-tools.exe info | ConvertFrom-Json).pbiSessions.ProcessId
+
+        if ($null -eq $PrId) {
+            Write-Output "`n"
+            throw ">>> Launch .pbix first, attach Watch Mode only after that..."
+        }
+
+        $this.WriteVerbose(">>> Watch Mode is on. Save report in PBI and see changes in a VS Code Git Tab")
+        $this.WriteVerbose("--> Ctrt + C to Quit Watch Mode")
+        
+        pbi-tools.exe extract -pid $PrId -watch
+    }
 
     [void] UpdateManagementPlanTables() {    
         <#
@@ -180,8 +202,8 @@ class PBIX {
             -WorksheetName "Planned Objects" `
             -StartRow 3
         $mgmPlanTables = $this.managementPlan `
-            | Where-Object { $_.'02_Type' -eq 'Table' } `
-            | Where-Object { $_.'08_Status' -ne 'Removed' }
+        | Where-Object { $_.'02_Type' -eq 'Table' } `
+        | Where-Object { $_.'08_Status' -ne 'Removed' }
             
         $objKeys = ($mgmPlanTables | Get-Member -MemberType NoteProperty).Name
 
@@ -229,7 +251,7 @@ Specification" | Set-Content $path
             # writing to the target file
             (Get-Content $path) -join "`n" `
                 -replace $pattern, ($required_qwr + $endingComma) `
-                | Set-Content $path
+            | Set-Content $path
         }        
     } # } UpdateManagementPlanTables
     
