ced1e895b0aa869651e89b765ad25f2912ad6a50
SR <sergiy.razumov@gmail.com>
2023-01-04 22:15:01 +0200

    SR [2023-01-04]: Refactoring possibility investigated:  >> Basic solution for addressing $var as a container for functions is found

--- PowerShell-Scripts/01_MainClass_Wrapper.ps1
+++ PowerShell-Scripts/01_MainClass_Wrapper.ps1
@@ -1,6 +1,9 @@
-function a {   
-}
-function b {   
-}
-function c {   
-}
+function a { cw 'writing a' }
+function b { cw 'writing b' }
+function c { cw 'writing c' } 
+
+# trick with [PsCustomObject] does not solve the issue, as I'm getting an Object again))) So, addressing its methods is totally the same as addressing $pbx.something()
+#this works. #TODO: check for parameters accuracy
+
+$func = @{}
+"a","b","c" | % {$func += @{$_ = Get-Command -Name $_}}
