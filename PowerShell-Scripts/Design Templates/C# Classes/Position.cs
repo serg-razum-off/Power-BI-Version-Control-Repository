793ea6d797d442ce742a032d5750611aa25d82de
SR <sergiy.razumov@gmail.com>
2023-01-21 19:13:39 +0200

    SR [2023-01-21]: Minor changes
    >>$PROFILE: creation of folder with nested file
    >>Posiiton.cs: class for holding position part of json

---
+++ PowerShell-Scripts/Design Templates/C# Classes/Position.cs
@@ -0,0 +1,9 @@
+class Position
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public int Width { get; set; }
+    public int Height { get; set; }
+    public int Z { get; set; }
+}
+
